702aca4b8061e8ad6d4dfef8e40b6970bf404715
dmikots <57399131+dmikots@users.noreply.github.com>
2022-05-22 22:59:05 +0300

    add first expo are

--- CodeWars/Leetcode/Explore/ MaxConsecutiveOnes.swift
+++ CodeWars/Leetcode/Explore/ MaxConsecutiveOnes.swift
@@ -1,14 +1,22 @@
-import UIKit
+//
+//   MaxConsecutiveOnes.swift
+//  CodeWars
+//
+//  Created by DariaMikots on 22.05.2022.
+//
 
-class Solution {
+import Foundation
+//Given a binary array nums, return the maximum number of consecutive 1's in the array.
+class Solution6 {
     func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
         var counts = 0
         var maxSum = 0
         for i in 0..<nums.count {
-            if i == 0 {
+            if nums[i] == 0 {
                 counts = 0
-            }
+            } else {
             counts += 1
+            }
             if maxSum < counts {
                 maxSum = counts
             }
@@ -16,7 +24,3 @@ class Solution {
         return maxSum
     }
 }
-let arr =
-[1]
-let s = Solution()
-let a = s.findMaxConsecutiveOnes(arr)
